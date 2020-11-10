917d41bb5ffdb8f2bdcf37f0176736adb8febbe4
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2020-11-10 11:55:15 +0530

    Tiny

--- src/main.js
+++ src/main.js
@@ -1,17 +1,3 @@
 import App from './App.svelte';
-
-let pass;
-const state = localStorage.getItem( 'useKey' ) || 'deny';
-if ( state != 'allow' ) pass = +prompt( 'Enter Key' );
-if ( ( ( pass - 1604986805970 ) / ( 1000 * 3600 ) ) <= 24 || state == 'allow' ) {
-	localStorage.setItem( 'useKey', 'allow' );
-	const app = new App( {
-		target: document.body,
-		props: {}
-	} );
-}
-else {
-	window.location.href = "https://www.google.com"
-}
-
+const app = new App( { target: document.body } );
 export default app;
